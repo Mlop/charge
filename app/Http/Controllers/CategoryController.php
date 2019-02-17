720b9bd787c7cd1c94ab1f75df2357658d04b243
qfkong <kong.qf@mdg-app.com>
2019-02-17 23:15:06 +0800

    add login and register

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -20,14 +20,45 @@ class CategoryController extends Controller
         $this->catRep = $catRep;
     }
 
-    public function get(Request $request)
+    public function getList(Request $request)
     {
         $type = $request->input('type', CategoryRepository::TYPE_IN);
         $parent_id = $request->input('parent_id', 0);
-        $list = $this->catRep->get($type, $parent_id);
+        $list = $this->catRep->getList($type, $parent_id);
         foreach ($list as $i => $cat) {
             $list[$i]['total'] = $this->catRep->count($type, $cat->id);
         }
         return $list;
     }
+	/**
+	添加或编辑类别
+	*/
+	public function edit(Request $request, $id)
+	{
+		$title = $request->input("title");
+		$parent_id = $request->input("parent_id");
+		$type = $request->input("type");
+		
+		//创建
+		if ($id == 0) {
+			$params = [
+				"title" => $title,
+				"parent_category_id" => $parent_id,
+				"type" => $type,
+			];
+			return $this->catRep->add($params);
+		} else {//编辑
+			$params = [
+				"title" => $title,
+			];
+			
+			$isOk = $this->catRep->edit($id, $params);
+			return $isOk ? ['code'=>0, 'msg'=>'修改成功'] : ['code'=>1, 'msg'=>'修改失败'];
+		}
+	}
+	public function del($id)
+	{
+		$isOk = $this->catRep->del($id);
+		return $isOk ? ['code'=>0, 'msg'=>'删除成功'] : ['code'=>1, 'msg'=>'删除失败'];
+	}
 }
