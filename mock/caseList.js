482aafa2551f7a6b991544556bf53fad04d38457
zhangchunhua <zhangchunhua@tourscool.com>
2019-05-06 16:39:52 +0800

    add mock data

---
+++ mock/caseList.js
@@ -0,0 +1,48 @@
+// 案件搜索列表
+var images = ['http://test.com/a.png','http://test.com/b.png','http://test.com/c.png'];
+Mock.mock('http://test.com', {
+    //1. 过滤条件
+    "filter": {
+        "cities|9":[
+            {
+                "id":"@natural(60, 100)",
+                "title":"@city",
+                "areas":[{
+                    "id":"@natural(60, 100)",
+                    "title":"@county"
+                }]
+            }],//可选择在城市
+        "selected":[],//选中的城市
+        "types|10":[{
+            "id":"@natural(60, 100)",
+            "title":"@ctitle"
+        }],
+        "status":[
+            {"id":"@natural(1, 20)","title":"可接案"},
+            {"id":"@natural(1, 20)","title":"已诉前调解"},
+            {"id":"@natural(1, 20)","title":"已立案"},
+            {"id":"@natural(1, 20)","title":"已开庭"},
+            {"id":"@natural(1, 20)","title":"已调解"},
+            {"id":"@natural(1, 20)","title":"已判决"},
+            {"id":"@natural(1, 20)","title":"已执行案件"},
+            {"id":"@natural(1, 20)","title":"已结案"}
+            ]
+    },
+    //2. 列表数据和分页
+    "items": {
+        "data|4": [{
+            "no": "@increment(1001)",//案件编号
+            "title": "@ctitle",//案件名称
+            "cycle": "@integer(60, 100)",//办案周期
+            "duration": "@integer(1, 10)",//合作周期
+            "total": "@integer(150, 400)",//合作案件总数
+            "cover|1": images//封面图片
+        }],
+        "pagination": {//分页
+            "total_page": "@integer(1, 10)",//总页数
+            "total_record": "@integer(1, 100)",//总记录数
+            "page": "@integer(1, 10)",//当前页
+            "page_size": 4,//每页记录数
+        }
+    }
+});
---
+++ mock/index.js
@@ -0,0 +1,31 @@
+//客户端首页数据
+Mock.mock('http://test.com', {
+    //1. 统计数据
+    "summary|4":[
+        {
+            "title":"@ctitle",//统计名称
+            "total|1000-9999":0//统计总数
+        }
+    ],
+    //2. 热门案件
+    "hot|9":[
+        {
+            "no":"@increment(1001)",//案件编号
+            "title":"@ctitle",//案件标题
+            "cycle":"@integer(60, 100)",//办案周期
+            "duration":"@integer(1, 10)",//合作周期
+            "total":"@integer(150, 400)",//合作案件总数
+            "price":"$@float(2000, 10000)",//价格
+        }
+    ],
+    //3. 合作企业
+    "companies|8":[
+        {
+            "duration":"@integer(1, 10)",//合作年数
+            "total":"@integer(150, 400)",//合作案件
+            "total_case":"@integer(150, 400)",//总案件数
+            "ing_case":"@integer(50, 400)",//进行中在案件
+            "avg_days":"@integer(1, 300)",//平均处理天数
+        }
+    ]
+});
