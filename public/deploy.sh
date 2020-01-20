b44c9dbbdaa421563102bbe764a45ab6c3cc1497
zhangchunhua <vera0228@sohu.com>
2020-01-20 13:58:18 +0800

    change exec

--- public/deploy.sh
+++ public/deploy.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
 LOG_FILE="/var/log/charge_deploy.log"
-
 date >> "$LOG_FILE"
 echo "Start deployment" >>"$LOG_FILE"
-cd /opt/www/charge
+cd /var/www/charge
 echo "pulling source code..." >> "$LOG_FILE"
 git pull origin master
-echo git log -1>>"$LOG_FILE"
+git log -1>>"$LOG_FILE"
 echo "Finished." >>"$LOG_FILE"
 echo >> $LOG_FILE
