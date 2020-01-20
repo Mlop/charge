b6061006e452ff9d75dee2486988c8e1e6a4ff98
zhangchunhua <vera0228@sohu.com>
2020-01-20 11:03:22 +0800

    test hook4

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,12 @@ $router->get('/', function () use ($router) {
 });
 //发布代码钩子
 $router->post('/deploy', function () use ($router) {
-//    system("deploy.sh");
+    system("deploy.sh");
 //    shell_exec("cd /opt/www/charge && git pull origin master && git log -1 2>&1");
-    system("cd /opt/www/charge;git pull origin master;git log -1;", $status);
-    var_dump($status);
+//    system("cd /opt/www/charge;git pull origin master;git log -1;", $status);
+//    var_dump($status);
     echo 'git pull finished';
-    echo '33';
+    echo ' 44';
 });
 //不需要登录接口
 $router->group(['middleware' => ['json_formatter']], function ($router) {
