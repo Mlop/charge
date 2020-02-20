863131093615afbe03f15b8ebe774aeea1d89bcd
zhangchunhua <vera0228@sohu.com>
2020-02-20 14:58:26 +0800

    fix book item

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -8,6 +8,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\BookItemRepository;
 use Illuminate\Http\Request;
 use App\Repositories\BookRepository;
 use Auth;
@@ -16,10 +17,12 @@ class BookController extends Controller
 {
     protected $bookRep;
     protected $userId;
+    protected $bookItemRep;
 
-    public function __construct(BookRepository $bookRep)
+    public function __construct(BookRepository $bookRep, BookItemRepository $bookItemRep)
     {
         $this->bookRep = $bookRep;
+        $this->bookItemRep = $bookItemRep;
         $user = Auth::user();
         $this->userId = $user ? $user->id : 0;
     }
@@ -59,4 +62,58 @@ class BookController extends Controller
 	{
 		return $this->bookRep->getList($this->userId);
 	}
+    /**
+     * 复选框列表数据
+     * @return mixed
+     */
+    public function getItemList(Request $request, $book_id)
+    {
+        $isIncludeUncheck = $request->get('is_include_uncheck', 1);
+        $defaultConfig = ['', '0.00', '0'];
+        $list = $this->bookItemRep->getList($book_id);
+        $data = [];
+        foreach ($list as $item) {
+            $data[] = ['value' => (string)$item['id'], 'name' => $item['title'], 'checked' => true, 'value_type'=>$item['value_type'], 'default_value'=>$defaultConfig[$item['value_type']]];
+        }
+        if ($isIncludeUncheck) {
+            $notSelect = $this->bookItemRep->getNotSelectedItems($book_id);
+            foreach ($notSelect as $item) {
+                $data[] = ['value' => (string)$item['id'], 'name' => $item['title'], 'checked' => false, 'value_type'=>$item['value_type'], 'default_value'=>$defaultConfig[$item['value_type']]];
+            }
+        }
+        return $data;
+    }
+
+    /**
+     * 选择和取消账本的列项
+     * @param Request $request
+     * @param $book_id
+     * @return array
+     */
+    public function checkItems(Request $request, $book_id)
+    {
+        $isCheck = $request->input('is_check', true);
+        $itemId = $request->input('item_id');
+        if ($isCheck) {
+            $params = [
+                "item_id" => $itemId,
+                "user_id" => $this->userId,
+                "book_id" => $book_id
+            ];
+            $isExists = $this->bookItemRep->exists($params);
+            if ($isExists) {
+                return ['code' => 1, 'msg' => '该条目已经存在'];
+            }
+            return $this->bookItemRep->add($params);
+        } else {
+            $params = [
+                "item_id" => $itemId,
+                "user_id" => $this->userId,
+                "book_id" => $book_id
+            ];
+            $isOk = $this->bookItemRep->deleteByParams($params);
+            return $isOk ? ['code'=>0, 'msg'=>'取消成功'] : ['code'=>1, 'msg'=>'取消失败'];
+        }
+        return ['code'=>0, 'msg'=>'操作成功'];
+    }
 }
