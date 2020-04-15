ad9d211fd036a1921e322313ae5fa81ad786b2ca
zhangchunhua <vera0228@sohu.com>
2020-04-15 11:18:10 +0800

    add cors

--- app/Http/Controllers/CommonController.php
+++ app/Http/Controllers/CommonController.php
@@ -21,17 +21,54 @@ class CommonController extends Controller
         return $this->contactRep->builder([])->pluck("name");
     }
 
+    private function cors()
+    {
+        $request_method = $_SERVER['REQUEST_METHOD'];
+        $origin = "*";
+        if ($request_method === 'OPTIONS') {
+
+            header('Access-Control-Allow-Origin:'.$origin);
+            header('Access-Control-Allow-Credentials:true');
+            header('Access-Control-Allow-Methods:GET, POST, OPTIONS');
+
+            header('Access-Control-Max-Age:1728000');
+            header('Content-Type:text/plain charset=UTF-8');
+            header('Content-Length: 0',true);
+
+            header('status: 204');
+            header('HTTP/1.0 204 No Content');
+
+        }
+
+        if ($request_method === 'POST') {
+
+            header('Access-Control-Allow-Origin:'.$origin);
+            header('Access-Control-Allow-Credentials:true');
+            header('Access-Control-Allow-Methods:GET, POST, OPTIONS');
+
+        }
+
+        if ($request_method === 'GET') {
+
+            header('Access-Control-Allow-Origin:'.$origin);
+            header('Access-Control-Allow-Credentials:true');
+            header('Access-Control-Allow-Methods:GET, POST, OPTIONS');
+
+        }
+    }
+
     /**
      * 上传文件到public目录
      */
     public function uploadFile(Request $request)
-    {var_dump('abc');exit;
+    {
+        $this->cors();
         $savePath = base_path()."/public/upload";
         if (!file_exists($savePath)) {
             mkdir($savePath);
             chmod($savePath, 0777);
         }
-        $file=$request->file('file');var_dump($file->isValid());exit;
+        $file=$request->file('file');
         $res = ['code' => 1, 'data'=>null, 'msg' => 'invalid file'];
         if ($file->isValid()) {
             $dir = $savePath;
@@ -39,7 +76,7 @@ class CommonController extends Controller
             $file->move( $dir, $filename);
 
             $res['code'] = 0;
-            $res['data'] = env("UPLOAD_URL").$filename;
+            $res['data'] = "http://".$filename;
             $res['msg'] = 'success';
         }
         return $res;
